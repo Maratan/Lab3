04c4c5f82f530bb145a8659db2f34d429ec7a7c9
Maratan <mart0n@mail.ru>
2016-10-13 17:45:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
 from uid import *
 from friends import *
 
@@ -8,5 +12,19 @@ user_id = user.execute()
 friends_client = GetFriends(user_id)
 friends = friends_client.execute()
 
+ages = []
+counts = []
+
 for (age, count) in friends:
     print('{} {}'.format(int(age), int(count)))
+    ages.append(int(age))
+    counts.append(int(count))
+
+plt.grid()
+plt.minorticks_on()
+plt.axis([0, 120, 0, 50])
+plt.figure(num=1, figsize=(8, 6))
+plt.xlabel('age', size=14)
+plt.ylabel('count', size=14)
+plt.bar(ages, counts, width=0.5)
+plt.show()
